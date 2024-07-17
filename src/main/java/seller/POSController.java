aa0e01a59289473522e690796cc4b72c6b40e920
yohan vishvajith <yohanvishvajith@gmail.com>
2024-07-17 18:50:53 +0530

    api fixed
    api and css error fixed

---
+++ src/main/java/seller/POSController.java
@@ -0,0 +1,45 @@
+package seller;
+
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class POSController{
+    @FXML
+    private Pane homePane;
+
+    @FXML
+    private Button btnLogin;
+
+    private  Stage stage;
+    public void btnOpenActionPerformed(){
+        TranslateTransition transition=new TranslateTransition();
+        transition.setNode(homePane);
+        transition.setByY(homePane.getLayoutY()-745);
+        transition.play();
+
+
+
+    }
+    public void btnLoginActionPerformed(ActionEvent event) throws IOException {
+
+
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        Parent root= FXMLLoader.load(POSapplication.class.getResource("Manager.fxml"));
+        Scene scene=new Scene(root);
+        String style=POSapplication.class.getResource("manager-style.css").toExternalForm();
+        scene.getStylesheets().add(style);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+}
---
+++ src/main/java/seller/addProductController.java
@@ -0,0 +1,29 @@
+package seller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class addProductController implements Initializable{
+    @FXML
+    private ChoiceBox<String> cbCatagory;
+
+    private String[] catagory={"Iphone","headphones","covers","powerbanks"};
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        cbCatagory.getItems().addAll(catagory);
+
+        cbCatagory.setOnAction((ActionEvent e)->{
+            System.out.println(cbCatagory.getValue());
+        });
+
+    }
+
+
+}
